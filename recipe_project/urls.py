68cdc320504a39f1582c8b4face582738e32c5c4
Cody-Ayers <122771685+Cody-Ayers@users.noreply.github.com>
2026-09-21 19:33:56 -0600

    *Serve media files

--- recipe_project/urls.py
+++ recipe_project/urls.py
@@ -15,11 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.static import serve
 from .views import login_view, logout_view
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -29,3 +29,11 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += [
+    re_path(
+        r'^media/(?P<path>.*)$',
+        serve,
+       *{'document_root': settings.MEDIA_R*OT},
+    ),
+]
